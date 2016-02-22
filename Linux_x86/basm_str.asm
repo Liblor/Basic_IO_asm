937849c5998190940f3a0a517edaa32f8cbdcc9f
Loris Reiff <loris.reiff@hotmail.com>
2016-02-22 15:35:04 +0100

    strcopy added

--- Linux_x86/basm_str.asm
+++ Linux_x86/basm_str.asm
@@ -3,6 +3,28 @@
 
 section .text
 
+    strcopy:
+        ; ecx source
+        ; edx destination
+        ; eax size (if 0 -> length of source)
+        push esi
+        push edi
+        mov esi, ecx
+        mov edi, edx
+        test eax, eax
+        jnz .copy
+        call strlen
+        inc eax
+      .copy:
+        mov ecx, eax
+        cld
+        rep movsb
+        dec eax
+        mov BYTE [edi+eax], 0
+        pop edi
+        pop esi
+        ret
+
     strlen:
         xor eax, eax
       .do:

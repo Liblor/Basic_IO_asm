12c8e17f797f58b9f81543c756be0f60e8240c16
Loris Reiff <loris.reiff@hotmail.com>
2016-02-24 11:22:11 +0100

    uitostr added

--- Linux_x86/basm_cast.asm
+++ Linux_x86/basm_cast.asm
@@ -3,7 +3,49 @@
 
 section .text
 
+%include "basm_str.asm"
+
+    itostr:
+        ; ecx -> int
+        ; edx -> address of buf
+        ; eax -> size of buf
+        ; TODO
+
+
+
+        ret
+
+    uitostr:
+        ; ecx -> unsigned int
+        ; edx -> address of buf
+        ; eax -> size of buf
+
+        push ebp
+        mov ebp, esp
+        push edx
+        sub esp, 12   ; 2^32, 10 digits + \0
+        mov BYTE [esp+11], 0x0
+        xchg eax, ecx
+        mov esi, 0x0a
+        mov edi, 0x0b
+      .loop:
+        dec edi
+        xor edx, edx
+        div esi
+        add dl, 0x30
+        mov BYTE [esp+edi], dl
+        test eax, eax
+        jne .loop
+        mov edx, [esp+12]
+        mov eax, ecx    ; size of buf
+        lea ecx, [esp+edi]
+        call strcopy
+        add esp, 16
+        leave
+        ret
+
     strtoi:
+    strtoui:
         xor esi, esi
         xor eax, eax
         xor edx, edx
